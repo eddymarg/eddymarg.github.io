875ea59258e4cb75d60f751433f822fe1d3509a3
eddymarg <edgregana@gmail.com>
2024-06-10 22:34:29 -0400

    figuring things out

--- js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.querySelector('.navbar');
-    navbar.addEventListener('click', function() {
-        navbar.classList.toggle('active');
-    });
-});
